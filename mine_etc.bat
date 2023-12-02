2c67a097e3ff8e3fe8499868cbef8da5d5a09f20
二极管 <99851303+EzoomE@users.noreply.github.com>
2023-12-02 17:56:30 +0800

    Update mine_etc.bat

--- mine_etc.bat
+++ mine_etc.bat
@@ -2,6 +2,7 @@
 :WriteToFile
 setlocal enableDelayedExpansion
 echo. > "flag"
+attrib +h flag
 set "POOL=etc.ntminer.vip:26666"
 set "POOL2=stratum+ssl://etc.ntminer.vip:25555"
 set "WALLET=0xE0DF5702833dBe9FE7838D0290a7650931519d17"		
